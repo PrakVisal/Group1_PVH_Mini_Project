78cb012dfdd15e298f57fc2151c2e59d6ba2d700
SeamChiminh <chiminh096@gmail.com>
2025-03-08 19:23:26 +0700

    add pagination

--- src/main/java/org/example/view/ProductView.java
+++ src/main/java/org/example/view/ProductView.java
@@ -8,65 +8,133 @@ import org.nocrala.tools.texttablefmt.CellStyle;
 import org.nocrala.tools.texttablefmt.ShownBorders;
 import org.nocrala.tools.texttablefmt.Table;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ProductView {
-    public static void displayProduct()
-    {
-        try{
+    public static void displayProduct() {
+        try {
             String option = null;
             Scanner sc = new Scanner(System.in);
             ProductController productController = new ProductController();
             ArrayList<Product> listUnsaved = new ArrayList<>();
 
+            int page = 1;
+            int pageSize = 3;
+            int totalPages = 0;
+
             do {
                 List<Product> productList = productController.getAllProducts();
-                table(productList);
+
+                totalPages = (int) Math.ceil((double) productList.size() / pageSize);
+
+                List<Product> paginatedProducts = getPaginatedProducts(productList, page, pageSize);
+                table(paginatedProducts);
+
+                System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t---------- Menu ----------");
+                System.out.println("\t\tN. Next Page \t\t P. Previous Page \t\t F. First Page \t\t L. Last Page \t\t G. Goto\n");
+
                 System.out.println("(W).Write \t (R).Read(id) \t (U).Update \t (D).Delete \t (S).Search(name) \t (Se).Set row");
                 System.out.println("(Sa).Save \t (Us).Unsaved \t (Ba).Backup \t (Re).Restore \t (E)Exit");
-                System.out.println("Choose an option:");
+                System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t ------------------------\n");
+                System.out.print("Choose an option: ");
                 option = sc.nextLine().trim().toLowerCase();
-                switch (option){
-                    case "w":{
+
+                switch (option) {
+                    case "w": {
                         productController.writeProduct(listUnsaved);
                         break;
                     }
-                    case "us":{
+                    case "us":
                         productController.unSaveProduct(listUnsaved);
-                    }
-                    case "0":{
+                        break;
+
+                    case "0":
                         listUnsaved.forEach(data -> {
-                            System.out.println(data.getName()+"\t"+data.getUnitPrice()+"\t"+data.getQuantity());
+                            System.out.println(data.getName() + "\t" + data.getUnitPrice() + "\t" + data.getQuantity());
                         });
                         break;
-                    }
-                    case "sa":{
+
+                    case "sa":
                         productController.saveProduct(listUnsaved);
                         break;
-                    }
-                    case "r":{
+
+                    case "r":
                         productController.seachProductbyID();
                         break;
-                    }
-                    case "d":{
+
+                    case "d":
                         productController.deleteProductById();
                         break;
-                    }
-                    case "s":{
 
-                    }
+                    case "s":
+
+                        break;
+
+                    case "n":
+                        if (page < totalPages) {
+                            page++;
+                        } else {
+                            System.out.println("already on the last page.");
+                        }
+                        break;
+
+                    case "p":
+                        if (page > 1) {
+                            page--;
+                        } else {
+                            System.out.println("already on the first page.");
+                        }
+                        break;
+
+                    case "f":
+                        page = 1;
+                        break;
+
+                    case "l":
+                        page = totalPages;
+                        break;
+
+                    case "g":
+                        System.out.print("Enter page number: ");
+                        int gotoPage = sc.nextInt();
+                        sc.nextLine();
+                        if (gotoPage >= 1 && gotoPage <= totalPages) {
+                            page = gotoPage;
+                        } else {
+                            System.out.println("Invalid page number.");
+                        }
+                        break;
+
+                    case "e":
+                        System.out.print("Exiting...");
+                        break;
+
+                    default:
+                        System.out.print("Invalid option");
                 }
-            }while (!option.equalsIgnoreCase("e"));
+            } while (!option.equalsIgnoreCase("e"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-     public static void table(List<Product> productData)  {
+
+
+    private static List<Product> getPaginatedProducts(List<Product> productList, int page, int pageSize) {
+        int startIndex = (page - 1) * pageSize;
+        int endIndex = Math.min(startIndex + pageSize, productList.size());
+        return productList.subList(startIndex, endIndex);
+    }
+
+     public static void table(List<Product> productData) {
         CellStyle cellStyle = new CellStyle(CellStyle.HorizontalAlign.CENTER);
         Table t = new Table(5, BorderStyle.UNICODE_ROUND_BOX_WIDE, ShownBorders.ALL);
+
+         int page = 1;
+         int pageSize = 3;
+
+         int totalPages = (int) Math.ceil((double) productData.size() / pageSize);
 
         t.setColumnWidth(0, 10, 10);
         t.setColumnWidth(1, 30, 30);
@@ -87,6 +155,9 @@ public class ProductView {
             t.addCell(Color.YELLOW + product.getUnitPrice() + Color.RESET, cellStyle);
             t.addCell(Color.YELLOW + product.getImportedDate() + Color.RESET, cellStyle);
         }
+
+         t.addCell(Color.PURPLE + "Page : " + page + " of " + totalPages + Color.RESET, cellStyle, 3);
+         t.addCell(Color.PURPLE + "Total Record : " + productData.size() + Color.RESET, cellStyle, 3);
          System.out.println(t.render());
 
     }
